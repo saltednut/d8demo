56117d36fcad10bd5e89dbf09d087376a9418dd8
brantwynn <brant.wynn@acquia.com>
2014-03-09 22:41:30 -0500

    removed unnessecary use statement from demo2 DemoController

--- demo2/lib/Drupal/demo2/Controller/DemoController.php
+++ demo2/lib/Drupal/demo2/Controller/DemoController.php
@@ -6,15 +6,12 @@
 
 namespace Drupal\demo2\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
-
 /**
  * Controller that accepts an argument.
  */
 class DemoController {
 
   public function demoPage($name) {
-    // Say "Hello, [name]!"
     $build = array(
       '#type' => 'markup',
       '#markup' => t('Hello, @name!', array('@name' => $name)),
