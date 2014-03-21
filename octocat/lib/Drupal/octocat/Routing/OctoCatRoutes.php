54fcac0b86eb5a24407853941afad3029c394a4a
brantwynn <brant.wynn@acquia.com>
2014-03-21 09:12:40 -0500

    Inject configFactory and encapsulate octocat.settings in its own method

--- octocat/lib/Drupal/octocat/Routing/OctoCatRoutes.php
+++ octocat/lib/Drupal/octocat/Routing/OctoCatRoutes.php
@@ -6,12 +6,31 @@
 
 namespace Drupal\octocat\Routing;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Routing\Route;
 
-class OctoCatRoutes {
+class OctoCatRoutes implements ContainerInjectionInterface {
+
+  protected $configFactory;
+
+  protected $route;
+
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('config.factory'));
+  }
+
+  private function octocatSettings() {
+    return $this->configFactory->get('octocat.settings');
+  }
 
   public function routes() {
-    $config = \Drupal::config('octocat.settings');
+    $config = $this->octocatSettings();
     $routes = array();
     foreach ($config->get('octocats') as $type) {
       $routes['octocat.page.' . $type] = new Route(
