5e14d57dc5a26e90ecee20f5eafbbed154e43d08
brantwynn <brant.wynn@acquia.com>
2014-05-22 09:31:50 -0500

    fix for OctoCatBlock create method

--- octocat/lib/Drupal/octocat/Plugin/Block/OctoCatBlock.php
+++ octocat/lib/Drupal/octocat/Plugin/Block/OctoCatBlock.php
@@ -51,7 +51,7 @@ class OctoCatBlock extends BlockBase implements ContainerFactoryPluginInterface 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, array $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
       $configuration,
       $plugin_id,
