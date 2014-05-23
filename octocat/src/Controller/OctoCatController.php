9626c9b2fdf89bef3f3e49452494e6287ea4cf53
brantwynn <brant.wynn@acquia.com>
2014-05-23 10:19:15 -0500

    escape input in octocat

--- octocat/src/Controller/OctoCatController.php
+++ octocat/src/Controller/OctoCatController.php
@@ -8,6 +8,7 @@ namespace Drupal\octocat\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Request;
+use Drupal\Component\Utility\String;
 
 /**
  * Controller for the dynamic routes.
@@ -23,7 +24,7 @@ class OctoCatController extends ControllerBase {
   public function page(Request $request, $type) {
     return array(
       '#theme' => 'octocat_display',
-      '#type' => $type,
+      '#type' =>  String::checkPlain($type),
     );
   }
 
